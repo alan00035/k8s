4d53a7956c94712f2088171620f8a68112f6f1a9
alan00035 <alan00035@hotmail.com>
2021-05-30 22:16:05 +1000

    Add files via upload

--- TF/variable.tf
+++ TF/variable.tf
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 variable "instance_name" {
   description = "value of the name tag for ec2 instance"
   type        = string
   default     = "example"
+=======
+variable "instance_name" {
+  description = "value of the name tag for ec2 instance"
+  type        = string
+  default     = "example"
+>>>>>>> ab9964bd3126413494bd7ddba0961797f69fb225
 }
