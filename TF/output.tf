86c3d9d25e7f9dfb2bcd68f44304f476643b3a4d
alan00035 <alan00035@hotmail.com>
2021-05-31 19:35:42 +1000

    update

--- TF/output.tf
+++ TF/output.tf
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 output "vpc_public_subnets"{
     description ="ids of public subnet"
     value = module.vpc.public_subnets
@@ -7,25 +7,5 @@ output "vpc_public_subnets"{
 output "ec2_instance_public_ips" {
     description ="public ips for ecs"
     value = module.ec2_instance.public_ips
-=======
-<<<<<<< HEAD
-output "vpc_public_subnets"{
-    description ="ids of public subnet"
-    value = module.vpc.public_subnets
-}
 
-output "ec2_instance_public_ips" {
-    description ="public ips for ecs"
-    value = module.ec2_instance.public_ips
-=======
-output "vpc_public_subnets"{
-    description ="ids of public subnet"
-    value = module.vpc.public_subnets
-}
-
-output "ec2_instance_public_ips" {
-    description ="public ips for ecs"
-    value = module.ec2_instance.public_ips
->>>>>>> 85966e3be5fff9320beb552a5d3649bd6a3a36f1
->>>>>>> ab9964bd3126413494bd7ddba0961797f69fb225
 }
--- TF/variable.tf
+++ TF/variable.tf
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
+
 variable "instance_name" {
   description = "value of the name tag for ec2 instance"
   type        = string
   default     = "example"
-=======
-<<<<<<< HEAD
-variable "instance_name" {
-  description = "value of the name tag for ec2 instance"
-  type        = string
-  default     = "example"
-=======
-variable "instance_name" {
-  description = "value of the name tag for ec2 instance"
-  type        = string
-  default     = "example"
->>>>>>> 85966e3be5fff9320beb552a5d3649bd6a3a36f1
->>>>>>> ab9964bd3126413494bd7ddba0961797f69fb225
+
 }
